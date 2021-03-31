533f5e19af9f23c8e6968ac38e5a21d0fb7a17ce
juliasilvacefet <79477170+juliasilvacefet@users.noreply.github.com>
2021-03-31 11:02:24 -0300

    Atualização para o programa admin

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -2,16 +2,23 @@
 const User = use("App/Models/User")
 
 class AuthController {
-    async register({request}){
-        const data = request.only(['username','email','password'])
-        const user = await User.create(data)
-        return user
+    try {
+      const data = request.only(["username", "email", "password"]);
+      const user = await User.create(data);
+      return user;
+    } catch (error) {
+      response.status(500).send("Erro ao cadastrar usuário!");
     }
 
     async authenticate({request,auth}){
-        const {email, password} = request.all()
-        const token = await auth.attempt(email,password)
-        return token
+        try {
+      const { email, password } = request.all();
+      const { token } = await auth.attempt(email, password);
+      const user = await User.findBy({ email });
+      return { token, user };
+    } catch (error) {
+      response.status(500).send("Login ou senha incorretos!");
+    }
     }
 }
 
