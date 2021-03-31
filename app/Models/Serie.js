a8282e033e4e39dea015eb7337cc729620251d6b
juliasilvacefet <79477170+juliasilvacefet@users.noreply.github.com>
2021-03-31 11:26:54 -0300

    Update Serie.js

--- app/Models/Serie.js
+++ app/Models/Serie.js
@@ -8,25 +8,6 @@ class Serie extends Model {
     genero(){
         return this.belongsTo("app/Models/Genero")
     }
-    
-    static get dates () {
-        return super.dates.concat(['lancamento'])
-      }
-    
-      static formatDates (field, value) {
-        if (field === 'lancamento') {
-            //Permite que a pessoa use a data no formato brasileiro
-          return Moment(value,'DD/MM/YYYY').format("YYYY-MM-DD")
-        }
-        return super.formatDates(field, value)
-      }
-
-      static castDates (field, value) {
-        if (field === 'lancamento') {
-          return Moment(value,'YYY-MM-DD').format('DD/MM/YYYY')
-        }
-        return super.formatDates(field, value)
-      }
 }
 
 module.exports = Serie
