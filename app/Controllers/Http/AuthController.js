05293fd07f32569a66ffc8b60213956a9f303100
juliasilvacefet <79477170+juliasilvacefet@users.noreply.github.com>
2021-03-31 11:11:58 -0300

    Update AuthController.js

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -2,23 +2,24 @@
 const User = use("App/Models/User")
 
 class AuthController {
-    try {
-      const data = request.only(["username", "email", "password"]);
-      const user = await User.create(data);
-      return user;
-    } catch (error) {
-      response.status(500).send("Erro ao cadastrar usuário!");
+   async register({ request, response }) {
+      try {
+        const data = request.only(["username", "email", "password"]);
+        const user = await User.create(data);
+        return user;
+      } catch (error) {
+        response.status(500).send("Erro ao cadastrar usuário!");
+      }
     }
-
     async authenticate({request,auth}){
-        try {
-      const { email, password } = request.all();
-      const { token } = await auth.attempt(email, password);
-      const user = await User.findBy({ email });
-      return { token, user };
-    } catch (error) {
-      response.status(500).send("Login ou senha incorretos!");
-    }
+      try {
+        const { email, password } = request.all();
+        const { token } = await auth.attempt(email, password);
+        const user = await User.findBy({ email });
+        return { token, user };
+      } catch (error) {
+        response.status(500).send("Login ou senha incorretos!");
+      }
     }
 }
 
